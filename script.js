12bfaec33bdb98d4c6ca4b0441cc1a039341bcdb
kulaizki <kulaizke@gmail.com>
2023-03-08 21:27:40 +0800

    feat: added player selection to scoreboard

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 const playButton = document.querySelector(".play");
 const one = document.querySelectorAll(".one");
 const two = document.querySelectorAll(".two");
-const selectionImages = document.querySelectorAll(".selection img");
 const clickAudio = document.getElementById("click-audio");
 const playAudio = document.getElementById("play-audio");
 
@@ -22,11 +21,34 @@ playButton.addEventListener("click", () => {
   })
 });
 
+const selectionImages = document.querySelectorAll(".selection img");
+
 selectionImages.forEach(img => {
   img.addEventListener("click", () => {
     clickAudio.currentTime = 0;
     clickAudio.play();
   });
+})
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const playerSelection = document.getElementById("player");
+const AiSelection = document.getElementById("ai");
+
+rock.addEventListener("click", () => {
+  playerSelection.src = "./images/rock.png";
+  playerSelection.style.height = "7.5rem";
+})
+
+paper.addEventListener("click", () => {
+  playerSelection.src = "./images/paper.png";
+  playerSelection.style.height = "7.5rem";
+})
+
+scissors.addEventListener("click", () => {
+  playerSelection.src = "./images/scissors.png";
+  playerSelection.style.height = "7.5rem";
 })
 
 // game();
@@ -40,29 +62,29 @@ selectionImages.forEach(img => {
 //     // play again
 //   }
 
-function getComputerChoice() {
-  let choice = Math.floor(Math.random() * 3 + 1);
+// function getComputerChoice() {
+//   let choice = Math.floor(Math.random() * 3 + 1);
 
-  switch (choice) {
-    case 1:
-      return "rock";
-    case 2:
-      return "paper";
-    case 3:
-      return "scissors";
-  }
-}
+//   switch (choice) {
+//     case 1:
+//       return "rock";
+//     case 2:
+//       return "paper";
+//     case 3:
+//       return "scissors";
+//   }
+// }
 
-function playRound(playerSelection, computerSelection) {
-  const results = {
-    rock: { scissors: "win", paper: "lose" },
-    paper: { rock: "win", scissors: "lose" },
-    scissors: { paper: "win", rock: "lose" },
-  };
+// function playRound(playerSelection, computerSelection) {
+//   const results = {
+//     rock: { scissors: "win", paper: "lose" },
+//     paper: { rock: "win", scissors: "lose" },
+//     scissors: { paper: "win", rock: "lose" },
+//   };
 
-  if (results[playerSelection] === undefined) return "Invalid Input";
-  if (playerSelection === computerSelection)
-    return `Its a draw! You both have ${playerSelection}`;
+//   if (results[playerSelection] === undefined) return "Invalid Input";
+//   if (playerSelection === computerSelection)
+//     return `Its a draw! You both have ${playerSelection}`;
 
-  return `You ${results[playerSelection][computerSelection]}! ${playerSelection} beats ${computerSelection}`;
-}
+//   return `You ${results[playerSelection][computerSelection]}! ${playerSelection} beats ${computerSelection}`;
+// }
